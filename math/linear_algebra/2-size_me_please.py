f2d06e72ab61bd1a03a7f0568751466d978aaae2
Megi96 <xibrrakumegi@gmail.com>
2025-10-16 16:31:54 +0000

    documnting and fixing style

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
+"""
+This module contains a function to calculate the shape of a matrix
+as a list of integers.
+"""
+
+
 def matrix_shape(matrix):
+    """
+        Calculate the shape of a nested list (matrix).
+
+        Args:
+            matrix (list): A nested list representing the matrix.
+
+        Returns:
+            list: A list of integers representing the size of each dimension.
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
