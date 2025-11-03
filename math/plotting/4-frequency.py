255d17a58be2b258895565b66da7bdbec67e402b
Megi96 <xibrrakumegi@gmail.com>
2025-11-03 22:31:26 +0000

    Adding task nr.4

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Create histogram
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+
+    # Add labels and title
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+
+    # Display plot
+    plt.show()
