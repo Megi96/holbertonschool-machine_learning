a3ebbbfef4b296db5441d17b0997ecadbe29cffd
Megi96 <xibrrakumegi@gmail.com>
2025-11-04 23:17:12 +0000

    Task 6

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -2,19 +2,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(5)
-fruit = np.random.randint(0, 20, (4, 3))
 
-names = ['Farrah', 'Fred', 'Felicia']
-fs = ['apples', 'bananas', 'oranges', 'peaches']
-colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+def bars():
+    """Plot a stacked bar chart of fruits per person."""
+    fruit = np.array([[12, 7, 5],   # apples
+                      [5, 10, 15],  # bananas
+                      [8, 5, 7],    # oranges
+                      [3, 2, 5]])   # peaches
 
-for i in range(len(fruit)):
-    plt.bar(names, fruit[i], bottom=np.sum(fruit[:i], axis=0),
-            color=colors[i], label=fs[i], width=0.5)
+    names = ['Farrah', 'Fred', 'Felicia']
+    fs = ['Apples', 'Bananas', 'Oranges', 'Peaches']
+    colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
 
-plt.title("Number of Fruit per Person")
-plt.ylabel("Quantity of Fruit")
-plt.yticks(np.arange(0, 90, 10))
-plt.legend()
-plt.show()
+    bottom = np.zeros(3)
+    for i in range(len(fruit)):
+        plt.bar(
+            names,
+            fruit[i],
+            bottom=bottom,
+            color=colors[i],
+            label=fs[i],
+            width=0.5
+        )
+        bottom += fruit[i]
+
+    plt.title("Number of Fruit per Person")
+    plt.ylabel("Quantity of Fruit")
+    plt.yticks(np.arange(0, 81, 10))
+    plt.ylim(0, 80)
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    bars()
