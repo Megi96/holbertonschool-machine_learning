dc41ecfe5c5db2ea43fe5943f05242dd9dab289e
Megi96 <xibrrakumegi@gmail.com>
2025-11-04 20:47:07 +0000

    Task nr.14 Adapting the bins to the task requests

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -11,8 +11,8 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    # Create histogram
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    # Create histogram with bins every 10 units (0–100 inclusive)
+    plt.hist(student_grades, bins=range(0, 110, 10), edgecolor='black')
 
     # Add labels and title
     plt.xlabel('Grades')
