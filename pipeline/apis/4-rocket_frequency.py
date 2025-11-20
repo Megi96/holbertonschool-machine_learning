ef053a7dcd78be95e7de260411736a0f955004ea
Megi96 <xibrrakumegi@gmail.com>
2025-11-20 21:44:31 +0000

    Adding Task nr 4

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -1,33 +1,44 @@
 #!/usr/bin/env python3
 """
-Displays the number of launches per rocket
+Script that displays the number of launches per rocket.
 """
+
 import requests
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # 1. Get all launches
+    launches = requests.get(
+        "https://api.spacexdata.com/v4/launches"
+    ).json()
 
-    rockets = {}
-
-    url = 'https://api.spacexdata.com/v4/launches'
-    r = requests.get(url)
-    launches = r.json()
+    # Count rocket occurrences
+    rocket_counts = {}
 
     for launch in launches:
-        rocket_id = launch['rocket']
-        url_r = "https://api.spacexdata.com/v4/rockets/{}".\
-            format(rocket_id)
-        req_r = requests.get(url_r)
-        json_r = req_r.json()
-        rocket_name = json_r['name']
+        rocket_id = launch.get("rocket")
+        if rocket_id:
+            rocket_counts[rocket_id] = rocket_counts.get(rocket_id, 0) + 1
 
-        if rocket_name in rockets.keys():
-            rockets[rocket_name] += 1
-        else:
-            rockets[rocket_name] = 1
+    # Replace rocket_id with rocket name
+    rocket_names = {}
+    for rocket_id in rocket_counts:
+        rocket_data = requests.get(
+            f"https://api.spacexdata.com/v4/rockets/{rocket_id}"
+        ).json()
+        name = rocket_data.get("name")
+        rocket_names[rocket_id] = name
 
-    sort = sorted(rockets.items(), key=lambda x: x[0])
-    sort = sorted(sort, key=lambda x: x[1], reverse=True)
+    # Build final list of (name, count)
+    final = []
+    for rocket_id, count in rocket_counts.items():
+        final.append((rocket_names[rocket_id], count))
 
-    for i in sort:
-        print("{}: {}".format(i[0], i[1]))
+    # Sort:
+    # 1) by count DESC
+    # 2) by name ASC for ties
+    final.sort(key=lambda x: (-x[1], x[0]))
+
+    # Print
+    for name, count in final:
+        print(f"{name}: {count}")
