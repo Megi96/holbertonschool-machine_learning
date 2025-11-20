b9aa39b9bd99afa366a28b998522c47886ed9a18
Megi96 <xibrrakumegi@gmail.com>
2025-11-20 21:51:08 +0000

    Adding Task nr.4

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -1,44 +1,26 @@
 #!/usr/bin/env python3
-"""
-Script that displays the number of launches per rocket.
-"""
-
+""" Get request from SpaceX"""
 import requests
 
 
-if __name__ == "__main__":
-    # 1. Get all launches
-    launches = requests.get(
-        "https://api.spacexdata.com/v4/launches"
-    ).json()
+if __name__ == '__main__':
 
-    # Count rocket occurrences
-    rocket_counts = {}
-
+    object = dict()
+    url = 'https://api.spacexdata.com/v4/launches'
+    launches = requests.get(url).json()
     for launch in launches:
-        rocket_id = launch.get("rocket")
-        if rocket_id:
-            rocket_counts[rocket_id] = rocket_counts.get(rocket_id, 0) + 1
+        urls = "https://api.spacexdata.com/v4/rockets/{}"
+        rocket_id = launch['rocket']
+        rocket_url = urls.format(rocket_id)
+        rocket_name = requests.get(rocket_url).json()['name']
 
-    # Replace rocket_id with rocket name
-    rocket_names = {}
-    for rocket_id in rocket_counts:
-        rocket_data = requests.get(
-            f"https://api.spacexdata.com/v4/rockets/{rocket_id}"
-        ).json()
-        name = rocket_data.get("name")
-        rocket_names[rocket_id] = name
+        if rocket_name in object.keys():
+            object[rocket_name] += 1
+        else:
+            object[rocket_name] = 1
 
-    # Build final list of (name, count)
-    final = []
-    for rocket_id, count in rocket_counts.items():
-        final.append((rocket_names[rocket_id], count))
+    keys = sorted(object.items(), key=lambda x: x[0])
+    keys = sorted(keys, key=lambda x: x[1], reverse=True)
 
-    # Sort:
-    # 1) by count DESC
-    # 2) by name ASC for ties
-    final.sort(key=lambda x: (-x[1], x[0]))
-
-    # Print
-    for name, count in final:
-        print(f"{name}: {count}")
+    for k in keys:
+        print("{}: {}".format(k[0], k[1]))
