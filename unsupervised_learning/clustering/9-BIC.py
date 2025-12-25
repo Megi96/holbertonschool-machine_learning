696922437f4ef2193a0d049101825bda4b3a2b47
Megi96 <xibrrakumegi@gmail.com>
2025-12-25 20:55:05 +0000

    Task 8

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python3
+"""Bayesian Information Criterion Module"""
+import numpy as np
+expectation_maximization = __import__('8-EM').expectation_maximization
+
+
+def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
+    """Finds the best number of clusters for a GMM using the Bayesian
+    Information Criterion:
+
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    kmin is a positive integer containing the minimum number of clusters to
+    check for (inclusive)
+    kmax is a positive integer containing the maximum number of clusters to
+    check for (inclusive)
+    If kmax is None, kmax should be set to the maximum number of clusters
+    possible
+    iterations is a positive integer containing the maximum number of
+    iterations for the EM algorithm
+    tol is a non-negative float containing the tolerance for the EM algorithm
     verbose is a boolean that determines if the EM algorithm should print
     information to the standard output
 
