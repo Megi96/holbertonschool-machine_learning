6624625376f311bd1207fabecb54624af9f6046c
Megi96 <xibrrakumegi@gmail.com>
2025-12-25 18:01:06 +0000

    Task 0

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -1,36 +1,47 @@
 #!/usr/bin/env python3
-"""Initialize K-means Module"""
+"""
+Defines function that initializes cluster centroids for K-means
+"""
+
+
 import numpy as np
 
 
 def initialize(X, k):
-    """Initializes cluster centroids for K-means:
-
-    X is a numpy.ndarray of shape (n, d) containing the dataset that will be
-    used for K-means clustering
-        n is the number of data points
-        d is the number of dimensions for each data point
-    k is a positive integer containing the number of clusters
-    The cluster centroids should be initialized with a multivariate uniform
-    distribution along each dimension in d:
-    The minimum values for the distribution should be the minimum values of X
-    along each dimension in d
-    The maximum values for the distribution should be the maximum values of X
-    along each dimension in d
-
-    Returns: a numpy.ndarray of shape (k, d) containing the initialized
-    centroids for each cluster, or None on failure
     """
+    Initializes cluster centroids for K-means
 
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    parameters:
+        X [numpy.ndarray of shape (n, d)]:
+            contains the dataset that will be used for K-means clustering
+            n: the number of data points
+            d: the number of dimensions for each data point
+        k [positive int]:
+            contains the number of clusters
+
+    cluster centroids initialized with a multivariate uniform distribution
+        along each dimension in d:
+        - minimum values for distribution should be the min values of X
+            along each dimension in d
+        - maximum values for distribution should be the max values of X
+            along each dimension in d
+        - should only use numpy.random.uniform exactly once
+
+    returns:
+        [numpy.ndarray of shape (k, d)]:
+            containing the initialized centroids for each cluster
+        or None on failure
+    """
+    # type checks to catch failure
+    if type(X) is not np.ndarray or len(X.shape) != 2:
         return None
-
-    _, d = X.shape
-
-    if not isinstance(k, int) or k <= 0:
+    if type(k) is not int or k <= 0:
         return None
-
-    centroids = np.random.uniform(low=np.min(
-        X, axis=0), high=np.max(X, axis=0), size=(k, d))
-
+    n, d = X.shape
+    # min values of X along each dimension in d
+    low = np.min(X, axis=0)
+    # max values of X along each dimension in d
+    high = np.max(X, axis=0)
+    # initialize cluster centroids with multivariate uniform distribution
+    centroids = np.random.uniform(low, high, size=(k, d))
     return centroids
