5da2abe7b5037dc4bb617a0967afc66587a716a9
Megi96 <xibrrakumegi@gmail.com>
2026-01-10 21:22:46 +0000

    Privatize neuron

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
+"""
+Module 1-neuron
+Defines a Neuron class for binary classification
+"""
+
 import numpy as np
 
 
 class Neuron:
-    """Defines a single neuron performing binary classification"""
+    """
+    Defines a single neuron performing binary classification
+    """
 
     def __init__(self, nx):
         """
-        nx: number of input features
+        Initializes a Neuron instance
+
+        Parameters:
+        nx (int): number of input features
+
+        Raises:
+        TypeError: if nx is not an integer
+        ValueError: if nx is less than 1
         """
         if not isinstance(nx, int):
             raise TypeError("nx must be a integer")
@@ -20,12 +34,30 @@ class Neuron:
 
     @property
     def W(self):
+        """
+        Getter for the weights vector
+
+        Returns:
+        numpy.ndarray: the weights of the neuron
+        """
         return self.__W
 
     @property
     def b(self):
+        """
+        Getter for the bias
+
+        Returns:
+        float: the bias value
+        """
         return self.__b
 
     @property
     def A(self):
+        """
+        Getter for the activated output
+
+        Returns:
+        float: the activation output of the neuron
+        """
         return self.__A
