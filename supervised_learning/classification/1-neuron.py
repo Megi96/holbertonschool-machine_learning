68f77efa964367d86a8da897f9f04f7bc9941611
Megi96 <xibrrakumegi@gmail.com>
2026-01-10 21:19:43 +0000

    Privatize Neuron

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """
+        nx: number of input features
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
