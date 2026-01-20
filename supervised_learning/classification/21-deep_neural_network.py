a8220ce071cc4998b08bb08d83b3cf9e4f9e741c
Megi96 <xibrrakumegi@gmail.com>
2026-01-20 15:43:02 +0000

    Task nr 21

--- supervised_learning/classification/21-deep_neural_network.py
+++ supervised_learning/classification/21-deep_neural_network.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
-"""DeepNeuralNetwork class performing binary classification"""
+"""
+Defines a deep neural network for binary classification
+"""
+
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """Defines a deep neural network performing binary classification"""
+    """
+    Deep neural network performing binary classification
+    """
 
     def __init__(self, nx, layers):
-        """Constructor
-        nx: number of input features
-        layers: list of number of nodes per layer
+        """
+        Constructor
+
+        nx (int): number of input features
+        layers (list): number of nodes in each layer
         """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
+
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
@@ -22,16 +30,24 @@ class DeepNeuralNetwork:
         self.__cache = {}
         self.__weights = {}
 
-        # Initialize weights/biases using single loop
-        for l in range(self.__L):
-            if not isinstance(layers[l], int) or layers[l] <= 0:
+        for index, nodes in enumerate(layers):
+            if not isinstance(nodes, int) or nodes <= 0:
                 raise TypeError("layers must be a list of positive integers")
-            w_key = "W" + str(l + 1)
-            b_key = "b" + str(l + 1)
-            input_size = nx if l == 0 else layers[l - 1]
-            # He et al. initialization
-            self.__weights[w_key] = np.random.randn(layers[l], input_size) * np.sqrt(2 / input_size)
-            self.__weights[b_key] = np.zeros((layers[l], 1))
+
+            layer_key = index + 1
+
+            if layer_key == 1:
+                self.__weights["W1"] = (
+                    np.random.randn(nodes, nx) * np.sqrt(2 / nx)
+                )
+            else:
+                prev_nodes = layers[index - 1]
+                self.__weights["W" + str(layer_key)] = (
+                    np.random.randn(nodes, prev_nodes)
+                    * np.sqrt(2 / prev_nodes)
+                )
+
+            self.__weights["b" + str(layer_key)] = np.zeros((nodes, 1))
 
     @property
     def L(self):
@@ -40,52 +56,74 @@ class DeepNeuralNetwork:
 
     @property
     def cache(self):
-        """Dictionary of all intermediary values"""
+        """Cache dictionary"""
         return self.__cache
 
     @property
     def weights(self):
-        """Dictionary of weights and biases"""
+        """Weights dictionary"""
         return self.__weights
 
     def forward_prop(self, X):
-        """Forward propagation using sigmoid"""
+        """
+        Calculates forward propagation
+
+        X (numpy.ndarray): input data of shape (nx, m)
+
+        Returns:
+        A (numpy.ndarray): output of the network
+        cache (dict): intermediary values
+        """
         self.__cache["A0"] = X
-        for l in range(self.__L):
-            W = self.__weights["W" + str(l + 1)]
-            b = self.__weights["b" + str(l + 1)]
-            A_prev = self.__cache["A" + str(l)]
-            Z = np.dot(W, A_prev) + b
+
+        for layer in range(1, self.__L + 1):
+            W = self.__weights["W" + str(layer)]
+            b = self.__weights["b" + str(layer)]
+            A_prev = self.__cache["A" + str(layer - 1)]
+
+            Z = np.matmul(W, A_prev) + b
             A = 1 / (1 + np.exp(-Z))
-            self.__cache["A" + str(l + 1)] = A
+
+            self.__cache["A" + str(layer)] = A
+
         return A, self.__cache
 
     def cost(self, Y, A):
-        """Logistic regression cost"""
+        """
+        Calculates cost using logistic regression
+
+        Y (numpy.ndarray): correct labels
+        A (numpy.ndarray): predicted labels
+        """
         m = Y.shape[1]
-        cost = -(1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        cost = -np.sum(
+            Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        ) / m
         return cost
 
-    def evaluate(self, X, Y):
-        """Evaluate predictions"""
-        A, _ = self.forward_prop(X)
-        return np.where(A >= 0.5, 1, 0), self.cost(Y, A)
-
     def gradient_descent(self, Y, cache, alpha=0.05):
-        """One step of gradient descent"""
-        m = Y.shape[1]
-        L = self.__L
-        dZ = None
+        """
+        Calculates one pass of gradient descent
 
-        for l in reversed(range(L)):
-            A = cache["A" + str(l + 1)]
-            A_prev = cache["A" + str(l)]
-            W = self.__weights["W" + str(l + 1)]
-            if l == L - 1:
-                dZ = A - Y
-            else:
-                dZ = np.dot(self.__weights["W" + str(l + 2)].T, dZ) * (A * (1 - A))
-            dW = np.dot(dZ, A_prev.T) / m
-            db = np.sum(dZ, axis=1, keepdims=True) / m
-            self.__weights["W" + str(l + 1)] -= alpha * dW
-            self.__weights["b" + str(l + 1)] -= alpha * db
+        Y (numpy.ndarray): correct labels
+        cache (dict): intermediary values
+        alpha (float): learning rate
+        """
+        m = Y.shape[1]
+        layers = self.__L
+
+        dZ = cache["A" + str(layers)] - Y
+
+        for layer in range(layers, 0, -1):
+            A_prev = cache["A" + str(layer - 1)]
+            W = self.__weights["W" + str(layer)]
+
+            dW = (1 / m) * np.matmul(dZ, A_prev.T)
+            db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+
+            self.__weights["W" + str(layer)] = W - alpha * dW
+            self.__weights["b" + str(layer)] -= alpha * db
+
+            if layer > 1:
+                A = cache["A" + str(layer - 1)]
+                dZ = np.matmul(W.T, dZ) * A * (1 - A)
