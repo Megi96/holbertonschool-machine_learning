ed7eab56506ba0caf7f9155c81f73303aade8ed7
Megi96 <xibrrakumegi@gmail.com>
2026-01-25 21:03:53 +0000

    Task 0

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
+"""
+0-sequential.py
+Builds a neural network using Keras Sequential API.
+"""
+
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
-    Builds a neural network using Keras
+    Builds a neural network using the Keras Sequential API.
+
+    Parameters:
+    - nx: number of input features
+    - layers: list containing the number of nodes in each layer
+    - activations: list containing activation functions for each layer
+    - lambtha: L2 regularization parameter
+    - keep_prob: probability of keeping a node for dropout
+
+    Returns:
+    - The Keras model
     """
     model = K.Sequential()
 
@@ -27,7 +42,6 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
                 )
             )
 
-        # Dropout only BETWEEN layers, not after the last one
         if keep_prob < 1 and i < len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
 
