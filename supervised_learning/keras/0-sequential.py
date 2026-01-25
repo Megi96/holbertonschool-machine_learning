1d70baa67a8ca13e53d6a98d956c36dc78b9c56e
Megi96 <xibrrakumegi@gmail.com>
2026-01-25 20:54:38 +0000

    Task 0

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import tensorflow.keras as K
 
 
@@ -6,15 +5,13 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     Builds a neural network using Keras
     """
-
     model = K.Sequential()
 
     for i in range(len(layers)):
         if i == 0:
-            # First layer needs input dimension
             model.add(
                 K.layers.Dense(
-                    layers[i],
+                    units=layers[i],
                     activation=activations[i],
                     kernel_regularizer=K.regularizers.l2(lambtha),
                     input_dim=nx
@@ -23,14 +20,14 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         else:
             model.add(
                 K.layers.Dense(
-                    layers[i],
+                    units=layers[i],
                     activation=activations[i],
                     kernel_regularizer=K.regularizers.l2(lambtha)
                 )
             )
 
-        # Add dropout after each layer
-        if keep_prob < 1:
+        # Dropout only BETWEEN layers, not after the last one
+        if keep_prob < 1 and i < len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
 
     return model
