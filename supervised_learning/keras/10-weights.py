d2215554f21bb0b1ee6cf68ffe00f365e679c9ac
Megi96 <xibrrakumegi@gmail.com>
2026-01-27 22:18:41 +0100

    Refactor docstrings for save and load weights functions

--- supervised_learning/keras/10-weights.py
+++ supervised_learning/keras/10-weights.py
@@ -1,36 +1,33 @@
 #!/usr/bin/env python3
 """
 10-weights.py
-Functions to save and load Keras model weights.
+Contains functions to save and load a Keras model's weights.
 """
 
 def save_weights(network, filename, save_format='keras'):
     """
-    Saves the weights of a Keras model.
+    Saves a model's weights to a file.
 
     Parameters:
-    - network: the Keras model whose weights should be saved
-    - filename: the path to save the weights
-    - save_format: 'keras' (HDF5) or 'tf' (TensorFlow checkpoint)
-    
-    Returns: None
+    network (keras.Model): the model whose weights should be saved
+    filename (str): path to the file where the weights will be saved
+    save_format (str): format in which the weights should be saved
+
+    Returns:
+    None
     """
-    # Validate save_format
-    if save_format not in ['keras', 'tf']:
-        raise ValueError("save_format must be 'keras' or 'tf'")
-    
-    # Save weights
     network.save_weights(filename, save_format=save_format)
 
 
 def load_weights(network, filename):
     """
-    Loads weights into a Keras model.
+    Loads weights into a model.
 
     Parameters:
-    - network: the Keras model to load weights into
-    - filename: the path from which to load the weights
-    
-    Returns: None
+    network (keras.Model): the model to which the weights will be loaded
+    filename (str): path to the file containing the weights
+
+    Returns:
+    None
     """
     network.load_weights(filename)
