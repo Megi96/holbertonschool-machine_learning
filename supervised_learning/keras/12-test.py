bf75863dc8b559d9233c7c5ead320851574fa944
Megi96 <xibrrakumegi@gmail.com>
2026-01-29 21:12:53 +0100

    Refine docstring in test_model function
    Updated docstring for test_model function to improve clarity.

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -1,24 +1,24 @@
-#Task 12
 #!/usr/bin/env python3
 """
 12-test.py
 Tests a neural network model
 """
+
 import tensorflow.keras as K
 
 
 def test_model(network, data, labels, verbose=True):
     """
-    Tests a neural network
+    Tests a neural network model.
 
     Parameters:
-    network: the model to test
-    data: input data
-    labels: one-hot labels
-    verbose: determines verbosity of output
+        network: the model to test
+        data: input data
+        labels: one-hot labels
+        verbose: determines verbosity of output
 
     Returns:
-    the loss and accuracy of the model, respectively
+        the loss and accuracy of the model, respectively
     """
     return network.evaluate(
         data,
