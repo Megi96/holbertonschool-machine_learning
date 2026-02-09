0d6f88bdfd2402ecfdf6bdda725de8317e3ffbe8
Megi96 <xibrrakumegi@gmail.com>
2026-02-09 21:46:04 +0100

    Refactor specificity function to calculate F1 score

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -1,28 +1,20 @@
 #!/usr/bin/env python3
 import numpy as np
 
-def specificity(confusion):
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
+
+def f1_score(confusion):
     """
-    Calculates the specificity for each class in a confusion matrix
+    Calculates the F1 score for each class in a confusion matrix
 
     Args:
         confusion: numpy.ndarray of shape (classes, classes)
 
     Returns:
-        numpy.ndarray of shape (classes,) containing specificity per class
+        numpy.ndarray of shape (classes,) containing F1 score per class
     """
-    classes = confusion.shape[0]
-    spec = np.zeros(classes)
+    recall = sensitivity(confusion)
+    prec = precision(confusion)
 
-    total = np.sum(confusion)
-
-    for i in range(classes):
-        TP = confusion[i, i]
-        FP = np.sum(confusion[:, i]) - TP
-        FN = np.sum(confusion[i, :]) - TP
-        TN = total - (TP + FP + FN)
-
-        spec[i] = TN / (TN + FP)
-
-
-    return spec
+    return 2 * (prec * recall) / (prec + recall)
