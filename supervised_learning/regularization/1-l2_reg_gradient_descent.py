5bb6a27a52b2e5fd22873f44e9ca50b3d687bbac
Megi96 <xibrrakumegi@gmail.com>
2026-02-14 21:08:27 +0100

    Refactor L2 regularization gradient descent function

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,26 +1,57 @@
 #!/usr/bin/env python3
-"""Gradient Descent with L2 Regularization"""
+"""Gradient descent with L2 regularization"""
 
 import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """Updates weights and biases using gradient descent with L2"""
+    """
+    Updates weights and biases of a neural network using gradient descent
+    with L2 regularization.
+
+    Parameters:
+    -----------
+    Y : numpy.ndarray of shape (classes, m)
+        one-hot encoded correct labels
+    weights : dict
+        contains W{l} and b{l} for l=1..L
+    cache : dict
+        contains A{l} for l=0..L (A0 = input data)
+    alpha : float
+        learning rate
+    lambtha : float
+        L2 regularization parameter
+    L : int
+        number of layers
+
+    Returns:
+    --------
+    None (updates weights in place)
+    """
     m = Y.shape[1]
-    dZ = cache["A" + str(L)] - Y
 
-    for i in reversed(range(1, L + 1)):
-        A_prev = cache["A" + str(i - 1)]
-        W = weights["W" + str(i)]
+    # Output layer gradient (softmax + cross-entropy → very simple)
+    dZ = cache['A' + str(L)] - Y
 
-        dW = (1 / m) * np.matmul(dZ, A_prev.T) + (lambtha / m) * W
+    for l in range(L, 0, -1):
+        A_prev = cache['A' + str(l - 1)]
+
+        # Weight gradient with L2 regularization term
+        dW = (1 / m) * np.matmul(dZ, A_prev.T)
+        dW += (lambtha / m) * weights['W' + str(l)]
+
+        # Bias gradient (no regularization on bias)
         db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
-        weights["W" + str(i)] = W - alpha * dW
-        weights["b" + str(i)] -= alpha * db
+        # Update weights and biases in place
+        weights['W' + str(l)] -= alpha * dW
+        weights['b' + str(l)] -= alpha * db
 
-        if i > 1:
-            A = cache["A" + str(i - 1)]
-            dZ = np.matmul(W.T, dZ) * (1 - np.square(A))
+        if l > 1:
+            # Backprop to previous layer
+            dA_prev = np.matmul(weights['W' + str(l)].T, dZ)
+            # tanh derivative
+            dZ = dA_prev * (1 - np.power(A_prev, 2))
+
 
             dZ = np.matmul(W.T, dZ) * (1 - np.square(A))
