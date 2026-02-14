de721dbcff249ac383293b7e90776fc88118290c
Megi96 <xibrrakumegi@gmail.com>
2026-02-14 22:08:00 +0100

    Refactor L2 regularization gradient descent function

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -5,31 +5,20 @@ import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """
-    Updates the weights and biases of a neural network using gradient descent
-    with L2 regularization (updates in place)
-    """
     m = Y.shape[1]
-
-    # Output layer gradient (softmax + cross-entropy)
     dz = cache['A' + str(L)] - Y
 
-    for l in range(L, 0, -1):
-        A_prev = cache['A' + str(l - 1)]
+    for layer in range(L, 0, -1):
+        A_prev = cache['A' + str(layer - 1)]
 
-        # Weight gradient + L2 regularization term
-        dW = np.matmul(dz, A_prev.T) / m
-        dW += (lambtha / m) * weights['W' + str(l)]
+        dW = (1 / m) * np.matmul(dz, A_prev.T)
+        dW += (lambtha / m) * weights['W' + str(layer)]
 
-        # Bias gradient (no L2 on bias)
-        db = np.sum(dz, axis=1, keepdims=True) / m
+        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
 
-        # Update in place
-        weights['W' + str(l)] -= alpha * dW
-        weights['b' + str(l)] -= alpha * db
+        weights['W' + str(layer)] -= alpha * dW
+        weights['b' + str(layer)] -= alpha * db
 
-        if l > 1:
-            # Backpropagate
-            dA_prev = np.matmul(weights['W' + str(l)].T, dz)
-            # tanh derivative – this exact form matches the checker
-            dz = dA_prev * (1 - A_prev * A_prev)
+        if layer > 1:
+            dA_prev = np.matmul(weights['W' + str(layer)].T, dz)
+            dz = dA_prev * (1 - A_prev * A_prev)   # ← try this first
