4ef7e9b47f7eb38f2a15bc9f533f842e28d1a8e6
Megi96 <xibrrakumegi@gmail.com>
2026-02-14 23:15:38 +0100

    Refactor L2 regularization gradient descent function

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,51 +1,29 @@
-#!/usr/bin/env python3
-"""
-Defines a function that updates the weights and biases
-using gradient descent with L2 Regularization
-"""
-
 import numpy as np
 
-
-def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+def l2_reg_gradient_descent(Y, P, weights, cache, alpha, lambtha, L):
     """
-    Updates weights and biases using gradient descent with L2 regularization
-
-    parameters:
-        Y [one-hot numpy.ndarray of shape (classes, m)]:
-            contains the correct labels for the data
-            classes: number of classes
-            m: number of data points
-        weights [dict]: dictionary of weights and biases for the network
-        cache [dict]: dictionary of the outputs of each layer of the network
-        alpha [float]: learning rate
-        lambtha: the regularization parameter
-        L: the number of layers in the neural network
-
-    Neural network using tanh activations on each layer except the last.
-    Last layer uses softmax activation.
+    Updates the weights and biases of a neural network using 
+    gradient descent with L2 regularization.
     """
     m = Y.shape[1]
-    back = {}
-    for index in range(L, 0, -1):
-        A = cache["A{}".format(index - 1)]
-        if index == L:
-            back["dz{}".format(index)] = (cache["A{}".format(index)] - Y)
-        else:
-            dz_prev = back["dz{}".format(index + 1)]
-            A_current = cache["A{}".format(index)]
-            back["dz{}".format(index)] = (
-                np.matmul(W_prev.transpose(), dz_prev) *
-                (A_current * (1 - A_current)))
-        dz = back["dz{}".format(index)]
-        dW = (1 / m) * (
-            (np.matmul(dz, A.transpose())) + (
-                lambtha * weights["W{}".format(index)]))
-        db = (1 / m) * (
-            (np.sum(dz, axis=1, keepdims=True)) + (
-                lambtha * weights["b{}".format(index)]))
-        W_prev = weights["W{}".format(index)]
-        weights["W{}".format(index)] = (
-            weights["W{}".format(index)] - (alpha * dW))
-        weights["b{}".format(index)] = (
-            weights["b{}".format(index)] - (alpha * db))
+    dZ = P - Y
+    
+    for i in range(L, 0, -1):
+        A_prev = cache[f'A{i-1}']
+        W = weights[f'W{i}']
+        b = weights[f'b{i}']
+        
+        # Calculate gradients
+        dW = (1 / m) * np.matmul(dZ, A_L_minus_1.T) + (lambtha / m) * W
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+        
+        # Calculate dZ for next iteration (backpropagate)
+        if i > 1:
+            A_prev = cache[f'A{i-1}']
+            dZ = np.matmul(W.T, dZ) * (1 - A_prev**2) # assuming tanh
+            
+        # Update weights and biases
+        weights[f'W{i}'] -= alpha * dW
+        weights[f'b{i}'] -= alpha * db
+        
+    return weights
