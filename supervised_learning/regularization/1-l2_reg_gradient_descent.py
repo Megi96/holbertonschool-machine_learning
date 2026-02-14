bc6df6d4723f8e88757fed4c5a38033869e8eedf
Megi96 <xibrrakumegi@gmail.com>
2026-02-14 23:23:18 +0100

    Update 1-l2_reg_gradient_descent.py

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python3
-"""Gradient descent with L2 regularization"""
+"""Write a function that updates the weights and
+biases of a neural network using gradient descent with L2 regularization"""
 
 import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """
-    Updates weights and biases using gradient descent with L2 regularization.
-    Updates happen in place.
-    """
+    """Update weights and biases using
+      gradient descent with L2 regularization"""
+
     m = Y.shape[1]
 
-    dz = cache['A' + str(L)] - Y
+    dZ = cache["A{}".format(L)] - Y
 
-    for layer in range(L, 0, -1):
-        A_prev = cache['A' + str(layer - 1)]
+    for i in range(L, 0, -1):
+        A_prev = cache["A{}".format(i - 1)]
+        W = weights["W{}".format(i)]
 
-        # Order that matches the reference exactly
-        dW = np.matmul(dz, A_prev.T) / m + weights['W' + str(layer)] * (lambtha / m)
+        dW = (1 / m) * np.matmul(dZ, A_prev.T) + (lambtha / m) * W
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
-        db = np.sum(dz, axis=1, keepdims=True) / m
+        weights["W{}".format(i)] = W - alpha * dW
+        weights["b{}".format(i)] = weights["b{}".format(i)] - alpha * db
 
-        weights['W' + str(layer)] -= alpha * dW
-        weights['b' + str(layer)] -= alpha * db
-
-        if layer > 1:
-            dA_prev = np.matmul(weights['W' + str(layer)].T, dz)
-            # This exact expression matches the checker's floating-point result
-            dz = dA_prev * (1 - A_prev**2)
+        if i > 1:
+            A_prev_layer = cache["A{}".format(i - 1)]
+            dA_prev = np.matmul(W.T, dZ)
+            dZ = dA_prev * (1 - np.square(A_prev_layer))
