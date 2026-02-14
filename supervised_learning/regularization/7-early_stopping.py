597c4479108dec1e7c0c60c811d7b62a76b7ffab
Megi96 <xibrrakumegi@gmail.com>
2026-02-14 20:49:49 +0100

    Implement early stopping function for training

---
+++ supervised_learning/regularization/7-early_stopping.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Early stopping"""
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """
+    Determines whether to stop training early
+    """
+
+    if opt_cost - cost > threshold:
+        count = 0
+    else:
+        count += 1
+
+    return count >= patience, count
