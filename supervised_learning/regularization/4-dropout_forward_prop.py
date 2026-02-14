e78e45b42a8ce459e4a4c700b504ad438e9b044f
Megi96 <xibrrakumegi@gmail.com>
2026-02-14 20:44:38 +0100

    Refactor dropout forward propagation implementation

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env python3
-"""Forward Propagation with Dropout"""
+"""Forward propagation with Dropout"""
 
 import numpy as np
 
 
 def dropout_forward_prop(X, weights, L, keep_prob):
-    """Performs forward propagation with dropout"""
+    """
+    Conducts forward propagation using Dropout
+    """
     cache = {}
     cache['A0'] = X
 
-    for l in range(1, L + 1):
-        W = weights['W' + str(l)]
-        b = weights['b' + str(l)]
-        A_prev = cache['A' + str(l - 1)]
+    for i in range(1, L + 1):
+        W = weights['W{}'.format(i)]
+        b = weights['b{}'.format(i)]
+        A_prev = cache['A{}'.format(i - 1)]
 
         Z = np.matmul(W, A_prev) + b
 
-        if l != L:
-            # Hidden layer: tanh + dropout
+        if i != L:
             A = np.tanh(Z)
-            D = np.random.rand(*A.shape) < keep_prob
-            A *= D
-            A /= keep_prob
-            cache['D' + str(l)] = D
-        else:
-            # Output layer: softmax
-            exp_Z = np.exp(Z - np.max(Z, axis=0, keepdims=True))
-            A = exp_Z / np.sum(exp_Z, axis=0, keepdims=True)
 
-        cache['A' + str(l)] = A
+            D = np.random.rand(A.shape[0], A.shape[1]) < keep_prob
+            A = A * D
+            A = A / keep_prob
+
+            cache['D{}'.format(i)] = D.astype(int)
+        else:
+            exp = np.exp(Z - np.max(Z, axis=0, keepdims=True))
+            A = exp / np.sum(exp, axis=0, keepdims=True)
+
+        cache['A{}'.format(i)] = A
 
     return cache
