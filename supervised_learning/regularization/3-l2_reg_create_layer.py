006a417b34dd25bbfb6486d49ea19c2d2ce635b2
Megi96 <xibrrakumegi@gmail.com>
2026-02-14 20:36:59 +0100

    Update layer creation with new initializer and docstring

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
-"""Create a Layer with L2 Regularization"""
+"""Create a layer with L2 regularization"""
 
 import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """Creates a Dense layer with L2 regularization"""
-    initializer = tf.keras.initializers.HeNormal()
+    """
+    Creates a TensorFlow layer with L2 regularization
+    """
+    initializer = tf.keras.initializers.VarianceScaling(
+        scale=2.0,
+        mode="fan_avg"
+    )
+
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
         kernel_initializer=initializer,
         kernel_regularizer=tf.keras.regularizers.L2(lambtha)
     )
+
     return layer(prev)
