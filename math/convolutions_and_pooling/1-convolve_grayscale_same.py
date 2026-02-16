591b94e7790d855b1db64d01721b869430801edb
Megi96 <xibrrakumegi@gmail.com>
2026-02-16 21:59:40 +0000

    Task 1

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -5,22 +5,29 @@ import numpy as np
 
 
 def convolve_grayscale_same(images, kernel):
+    """
+    Performs a same convolution on grayscale images
+
+    images: numpy.ndarray of shape (m, h, w)
+    kernel: numpy.ndarray of shape (kh, kw)
+
+    Returns: numpy.ndarray containing convolved images
+             of shape (m, h, w)
+    """
+
     m, h, w = images.shape
     kh, kw = kernel.shape
 
     # Flip kernel for true convolution
     kernel = np.flip(kernel, axis=(0, 1))
 
-    p_h = kh - 1
-    p_w = kw - 1
-
+    # SAME padding calculation (works for odd & even kernels)
     pad_top = (kh - 1) // 2
     pad_bottom = kh - 1 - pad_top
-
     pad_left = (kw - 1) // 2
     pad_right = kw - 1 - pad_left
 
-
+    # Pad images with zeros
     padded = np.pad(
         images,
         ((0, 0),
@@ -29,11 +36,16 @@ def convolve_grayscale_same(images, kernel):
         mode='constant'
     )
 
+    # Initialize output
     output = np.zeros((m, h, w))
 
+    # Only two loops allowed
     for i in range(h):
         for j in range(w):
             image_slice = padded[:, i:i+kh, j:j+kw]
-            output[:, i, j] = np.sum(image_slice * kernel, axis=(1, 2))
+            output[:, i, j] = np.sum(
+                image_slice * kernel,
+                axis=(1, 2)
+            )
 
     return output
