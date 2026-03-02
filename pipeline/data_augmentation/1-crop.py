c2a5da49ab43982b2066117f4c783f5759c5069c
Megi96 <xibrrakumegi@gmail.com>
2026-03-02 21:55:37 +0000

    Task 1

---
+++ pipeline/data_augmentation/1-crop.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+
+def crop_image(image, size):
+    """
+    Performs a random crop of an image.
+
+    Args:
+        image: 3D tf.Tensor (height, width, channels)
+        size: tuple (new_height, new_width, channels)
+
+    Returns:
+        Cropped 3D tf.Tensor
+    """
+    return tf.image.random_crop(image, size=size)
