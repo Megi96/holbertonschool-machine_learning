d58e455d8ac159666d9b87d5d59e2625cea4148f
Megi96 <xibrrakumegi@gmail.com>
2026-03-02 21:51:50 +0000

    Task 0

---
+++ pipeline/data_augmentation/0-flip.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+
+def flip_image(image):
+    """
+    Flips an image horizontally.
+
+    Args:
+        image: 3D tf.Tensor (height, width, channels)
+
+    Returns:
+        Flipped 3D tf.Tensor
+    """
+    return tf.image.flip_left_right(image)
