646e8e87929e905dabafdd4ba902a57da5b85a77
Megi96 <xibrrakumegi@gmail.com>
2026-04-12 14:46:31 +0000

    Tasks fixed

--- supervised_learning/object_detection/7-yolo.py
+++ supervised_learning/object_detection/7-yolo.py
@@ -173,10 +173,12 @@ class Yolo:
             cls_boxes = cls_boxes[order]
             cls_scores = cls_scores[order]
 
-            keep = []
+            kept_boxes = []
+            kept_scores = []
 
             while len(cls_boxes) > 0:
-                keep.append(0)
+                kept_boxes.append(cls_boxes[0])
+                kept_scores.append(cls_scores[0])
 
                 if len(cls_boxes) == 1:
                     break
@@ -206,11 +208,14 @@ class Yolo:
                 cls_boxes = cls_boxes[1:][suppress_mask]
                 cls_scores = cls_scores[1:][suppress_mask]
 
-            box_predictions.append(cls_boxes[:len(keep)])
+            kept_boxes = np.array(kept_boxes)
+            kept_scores = np.array(kept_scores)
+
+            box_predictions.append(kept_boxes)
             predicted_box_classes.append(
-                np.full(len(keep), cls, dtype=box_classes.dtype)
+                np.full(len(kept_boxes), cls, dtype=box_classes.dtype)
             )
-            predicted_box_scores.append(cls_scores[:len(keep)])
+            predicted_box_scores.append(kept_scores)
 
         box_predictions = np.concatenate(box_predictions, axis=0)
         predicted_box_classes = np.concatenate(
