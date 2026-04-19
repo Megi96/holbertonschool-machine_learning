4c1e2d6db6247d64d35862d7970e096f51bd51ea
Megi96 <xibrrakumegi@gmail.com>
2026-04-19 18:00:59 +0000

    Task 6 fixed output

--- supervised_learning/RNNs/6-bi_backward.py
+++ supervised_learning/RNNs/6-bi_backward.py
@@ -7,12 +7,25 @@ class BidirectionalCell:
     Bidirectional RNN cell
     """
 
+    def __init__(self, i, h, o):
+        """
+        Initialize weights and biases
+        """
+
+        self.Whf = np.random.randn(i + h, h)
+        self.Whb = np.random.randn(i + h, h)
+        self.Wy = np.random.randn(i + 2 * h, o)
+
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        concat = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.matmul(concat, self.Whf) + self.bhf)
+        return h_next
+
     def backward(self, h_next, x_t):
-        """
-        Backward hidden state computation
-        """
-
-        h_prev = np.matmul(x_t, self.Whb) + np.matmul(h_next, self.Wbh) + self.bhb
-        h_prev = np.tanh(h_prev)
-
+        concat = np.concatenate((h_next, x_t), axis=1)
+        h_prev = np.tanh(np.matmul(concat, self.Whb) + self.bhb)
         return h_prev
