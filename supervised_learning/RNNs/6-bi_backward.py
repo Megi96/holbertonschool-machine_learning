19c21dbe8e92655d04e2eb21a1d709ce1a1d0c7b
Megi96 <xibrrakumegi@gmail.com>
2026-04-19 17:58:22 +0000

    Task 6 and 7 fixed version- importing only numpy

--- supervised_learning/RNNs/6-bi_backward.py
+++ supervised_learning/RNNs/6-bi_backward.py
@@ -1,33 +1,18 @@
 #!/usr/bin/env python3
 import numpy as np
-BidirectionalCell = __import__('5-bi_forward').BidirectionalCell
 
 
-class BidirectionalCell(BidirectionalCell):
+class BidirectionalCell:
     """
-    Represents a bidirectional cell of an RNN
+    Bidirectional RNN cell
     """
 
     def backward(self, h_next, x_t):
         """
-        Calculates the hidden state in the backward direction
-        for one time step
-
-        Args:
-            h_next (numpy.ndarray): shape (m, h)
-                next hidden state
-            x_t (numpy.ndarray): shape (m, i)
-                input at time step t
-
-        Returns:
-            h_prev (numpy.ndarray): shape (m, h)
-                previous hidden state
+        Backward hidden state computation
         """
 
-        # linear combination of input and next hidden state
-        h_prev = np.matmul(x_t, self.Whb) + np.matmul(h_next, self.bhb) + self.bhb
-
-        # apply activation
+        h_prev = np.matmul(x_t, self.Whb) + np.matmul(h_next, self.Wbh) + self.bhb
         h_prev = np.tanh(h_prev)
 
         return h_prev
--- supervised_learning/RNNs/7-bi_output.py
+++ supervised_learning/RNNs/7-bi_output.py
@@ -1,42 +1,30 @@
 #!/usr/bin/env python3
 import numpy as np
-BidirectionalCell = __import__('6-bi_backward').BidirectionalCell
 
 
-class BidirectionalCell(BidirectionalCell):
+class BidirectionalCell:
     """
     Bidirectional RNN cell
     """
 
     def output(self, H):
         """
-        Calculates all outputs for the RNN
-
-        Args:
-            H (numpy.ndarray): shape (t, m, 2 * h)
-                concatenated hidden states (forward + backward)
-
-        Returns:
-            Y (numpy.ndarray): shape (t, m, o)
-                output probabilities
+        Compute outputs from hidden states
         """
 
         t, m, _ = H.shape
 
-        Y = []
+        outputs = []
 
         for i in range(t):
             h_t = H[i]
 
-            # linear transformation
             y_linear = np.matmul(h_t, self.Wy) + self.by
 
-            # numerical stability fix
-            y_exp = np.exp(y_linear - np.max(y_linear, axis=1, keepdims=True))
+            # stable softmax
+            exp = np.exp(y_linear - np.max(y_linear, axis=1, keepdims=True))
+            y = exp / np.sum(exp, axis=1, keepdims=True)
 
-            # softmax
-            y = y_exp / np.sum(y_exp, axis=1, keepdims=True)
+            outputs.append(y)
 
-            Y.append(y)
-
-        return np.array(Y)
+        return np.array(outputs)
