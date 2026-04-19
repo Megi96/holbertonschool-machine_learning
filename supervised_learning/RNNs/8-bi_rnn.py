affbe058ea4f25344f74694eb0855f7e7e0a64f4
Megi96 <xibrrakumegi@gmail.com>
2026-04-19 18:29:07 +0000

    Task 8

--- supervised_learning/RNNs/8-bi_rnn.py
+++ supervised_learning/RNNs/8-bi_rnn.py
@@ -1,43 +1,44 @@
 #!/usr/bin/env python3
+"""Module for bidirectional RNN forward propagation"""
 import numpy as np
 
 
-def bi_rnn(bi_cell, X, h_0, h_T):
+def bi_rnn(bi_cell, X, h_0, h_t):
     """
-    Bidirectional RNN forward propagation
+    Performs forward propagation for a bidirectional RNN
+
+    Args:
+        bi_cell: instance of BidirectionalCell used for forward propagation
+        X: numpy.ndarray of shape (t, m, i) containing the data
+           t is the maximum number of time steps
+           m is the batch size
+           i is the dimensionality of the data
+        h_0: numpy.ndarray of shape (m, h) containing the initial hidden
+             state in the forward direction
+        h_t: numpy.ndarray of shape (m, h) containing the initial hidden
+             state in the backward direction
+
+    Returns:
+        H: numpy.ndarray containing all concatenated hidden states
+        Y: numpy.ndarray containing all outputs
     """
+    t, m, _ = X.shape
+    _, h = h_0.shape
 
-    t, m, i = X.shape
-    h = h_0.shape[1]
+    H_forward = np.zeros((t, m, h))
+    H_backward = np.zeros((t, m, h))
 
-    # hidden states for forward and backward
-    Hf = np.zeros((t, m, h))
-    Hb = np.zeros((t, m, h))
-
-    # OUTPUT hidden states (concatenated)
-    H = np.zeros((t, m, 2 * h))
-
-    # forward initial state
     h_prev = h_0
-
-    # forward pass
     for step in range(t):
         h_prev = bi_cell.forward(h_prev, X[step])
-        Hf[step] = h_prev
+        H_forward[step] = h_prev
 
-    # backward initial state
-    h_next = h_T
-
-    # backward pass (reverse time)
+    h_next = h_t
     for step in reversed(range(t)):
         h_next = bi_cell.backward(h_next, X[step])
-        Hb[step] = h_next
+        H_backward[step] = h_next
 
-    # concatenate forward + backward
-    for step in range(t):
-        H[step] = np.concatenate((Hf[step], Hb[step]), axis=1)
-
-    # output layer
+    H = np.concatenate((H_forward, H_backward), axis=2)
     Y = bi_cell.output(H)
 
     return H, Y
