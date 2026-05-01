6684204b9b53196a02030decc1e0226c1e1e976d
Megi 96 <xibrrakumegi@gmail.com>
2026-05-01 20:18:59 +0000

    Task 2

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Train a Word2Vec model
+NLP --WE --Task 2
 """
 
 import gensim
@@ -10,27 +10,28 @@ def word2vec_model(sentences, vector_size=100, min_count=5,
                    window=5, negative=5, cbow=True,
                    epochs=5, seed=0, workers=1):
     """
-    Creates, builds and trains a gensim Word2Vec model
     """
-
+    # Set the training algorithm based on cbow parameter
     sg = 0 if cbow else 1
 
+    # Create the Word2Vec model
     model = gensim.models.Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
-        window=window,
         min_count=min_count,
+        window=window,
         negative=negative,
         sg=sg,
+        epochs=epochs,
         seed=seed,
         workers=workers
-    )
+        )
 
-    # IMPORTANT: explicit training (this is what checker expects)
-    model.train(
-        sentences,
-        total_examples=model.corpus_count,
-        epochs=epochs
-    )
+    # prepare the model vocabulary
+    model.build_vocab(sentences)
+
+    # Train the model
+    model.train(sentences, total_examples=model.corpus_count,
+                epochs=model.epochs)
 
     return model
